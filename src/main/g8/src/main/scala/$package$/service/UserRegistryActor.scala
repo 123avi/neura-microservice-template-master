ecc7587478da243844df3778534b6959b61db673
Avi Levi <123avi@gmail.com>
2019-08-09 13:40:02 +0300

    minor

--- src/main/g8/src/main/scala/$package$/service/UserRegistryActor.scala
+++ src/main/g8/src/main/scala/$package$/service/UserRegistryActor.scala
@@ -28,7 +28,6 @@ class UserRegistryActor extends Actor with ActorLogging {
       sender() ! Users(users.toSeq)
     case CreateUser(user) =>
       users += user
-//      sender() ! ActionPerformed(s"User ${user.name} created.")
 
     case GetUser(name) =>
       sender() ! users.find(_.name == name)
